fbf1033915fbbc474967eaefb0407ad7ee1df8a2
LeonardoRegalado <leonardoalev@gmail.com>
2024-08-29 21:05:52 -0600

    Registro del proyecto

---
+++ src/main/java/com/escom/proyect/EscomApplication.java
@@ -0,0 +1,13 @@
+package com.escom.proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscomApplication.class, args);
+	}
+
+}
